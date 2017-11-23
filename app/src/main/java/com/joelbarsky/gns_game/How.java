99d3e114ba204786cc98fd9dae348fc767a9ef83
Kevin Xie <kevinz1.kx@gmail.com>
2017-11-23 12:25:53 -0500

    -cosmetic changes /TODO How activity

---
+++ app/src/main/java/com/joelbarsky/gns_game/How.java
@@ -0,0 +1,17 @@
+package com.joelbarsky.gns_game;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+
+
+public class How extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how);
+
+    }
+}
